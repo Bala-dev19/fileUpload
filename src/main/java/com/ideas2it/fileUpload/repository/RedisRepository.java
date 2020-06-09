692db6f6b13130277fb44d6e0b1cf44566781010
Bala-dev19 <balakumaran@ideas2it.com>
2020-06-09 18:16:05 +0530

    Feature: redis

---
+++ src/main/java/com/ideas2it/fileUpload/repository/RedisRepository.java
@@ -0,0 +1,27 @@
+package com.ideas2it.fileUpload.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class RedisRepository extends GenericRepository {
+
+    /**
+     * To save the context for the dialog flow
+     * @param key - is the key to be saved
+     * @param value - is the value to be saved
+     */
+    public void saveContext(String key, String value) {
+        save(key, value);
+    }
+
+    /**
+     *
+     * To retrieved the context for the id
+     * @param key - is the key to be retrieved
+     * @return String - value of the key
+     */
+    public String fetchContextById(String key){
+        return (String) findById(key);
+    }
+
+}
